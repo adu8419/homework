b43cbb4a7893c14ee4890a0a3b1b748e80b39a7e
duyuanli <adu8419@163.com>
2022-11-21 22:56:05 +0800

    '修改增加555'
    '修改增加aaaa'

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -11,10 +11,10 @@ type Props = {
 const allOptions: Option[] = [
   {label: 'aaa', value: '111444',},
   {label: 'bbb', value: '222',},
-  {label: 'ccc', value: '333',},
+  {label: 'ccc', value: '3335555',},
   {label: 'ddd', value: '444',},
   {label: 'eee', value: '555',},
-  {label: 'fff', value: '666',},
+  {label: 'fff', value: '666aaaa',},
   {label: 'ggg', value: '777',},
   {label: 'hhh', value: '888',},
   {label: 'iii', value: '9990000',},
